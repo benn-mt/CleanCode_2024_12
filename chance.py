56053d3d557dd1c25b82eb224b2f42102e03f299
Ben Noble <ben.noble@madetech.com>
2024-12-09 14:28:20 +0000

    tests for not passing

--- chance.py
+++ chance.py
@@ -1,7 +1,12 @@
 class Chance:
+    __CERTAIN_LIKELIHOOD = 1.0
+
     def __init__(self, likelihood):
         self._likelihood = likelihood
 
     # override == operator
     def __eq__(self, other):
         return self._likelihood == other._likelihood
+
+    def Not(self):
+        return Chance(self.__CERTAIN_LIKELIHOOD - self._likelihood)
--- test_chance.py
+++ test_chance.py
@@ -1,5 +1,14 @@
 from chance import Chance
 
+CERTAIN = Chance(1)
+IMPOSSIBLE = Chance(0)
+FIFTY_FIFTY = Chance(0.5)
+
 def test_chances_are_equal_if_created_with_equal_likelihoods():
     assert Chance(1) == Chance(1)
     assert not Chance(1) == Chance(0)
+
+def test_not_is_inverse_of_probablility():
+    assert FIFTY_FIFTY == FIFTY_FIFTY.Not()
+    assert CERTAIN == IMPOSSIBLE.Not()
+    assert IMPOSSIBLE == IMPOSSIBLE.Not().Not()
