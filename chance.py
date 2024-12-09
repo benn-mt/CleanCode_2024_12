b96935e43fd840f61f6bac41c874488197e792bc
Ben Noble <ben.noble@madetech.com>
2024-12-09 14:35:24 +0000

    Added a threshold to deal with rounding errors

--- chance.py
+++ chance.py
@@ -1,12 +1,13 @@
 class Chance:
     __CERTAIN_LIKELIHOOD = 1.0
+    __THRESHOLD = 0.000001
 
     def __init__(self, likelihood):
         self._likelihood = likelihood
 
     # override == operator
     def __eq__(self, other):
-        return self._likelihood == other._likelihood
+        return abs(self._likelihood - other._likelihood) < self.__THRESHOLD
 
     def Not(self):
         return Chance(self.__CERTAIN_LIKELIHOOD - self._likelihood)
--- test_chance.py
+++ test_chance.py
@@ -12,3 +12,5 @@ def test_not_is_inverse_of_probablility():
     assert FIFTY_FIFTY == FIFTY_FIFTY.Not()
     assert CERTAIN == IMPOSSIBLE.Not()
     assert IMPOSSIBLE == IMPOSSIBLE.Not().Not()
+    assert Chance(0.4).Not() == Chance(0.6)
+    assert Chance(1.0 / 3.0).Not() == Chance(2.0 / 3.0)
