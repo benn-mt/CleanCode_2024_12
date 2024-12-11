d19bea93f4a16fd08046eddd21795ff2d159c7f3
Ben Noble <ben.noble@madetech.com>
2024-12-11 09:26:52 +0000

    Equality working for volumes

--- Quantities/test_quantity.py
+++ Quantities/test_quantity.py
@@ -27,3 +27,8 @@ def test_examples_from_exercise_with_s_method():
     assert Volumes.PINT.s(1) == Volumes.CUP.s(2)
     assert Volumes.QUART.s(1) == Volumes.PINT.s(2)
     assert Volumes.GALLON.s(1) == Volumes.QUART.s(4)
+
+def test_random_examples():
+    assert Volumes.TABLESPOON.s(2) == Volumes.TEASPOON.s(6)
+    assert Volumes.TABLESPOON.s(0.5) == Volumes.TEASPOON.s(1.5)
+    assert Volumes.GALLON.s(1) == Volumes.TEASPOON.s(4*2*2*8*2*3)
